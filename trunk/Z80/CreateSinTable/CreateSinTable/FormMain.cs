cf685b2ec17513fe48be25756e41e1cbdefcfd78
svpetry@gmail.com <svpetry@gmail.com@e77ae2b0-8bf4-09a1-894d-7d126f6b5e59>
2010-10-09 08:26:02 +0000

    Added sinus table generator

---
+++ trunk/Z80/CreateSinTable/CreateSinTable/FormMain.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TextWriter tw = new StreamWriter("sintable.txt");
+
+            int i;
+            for (i = 0; i < 256; i++)
+            {
+                tw.Write((int)(127 * Math.Sin(i * 2 * Math.PI / 256)));
+                if (i < 255)
+                    tw.Write(", ");
+                if (i % 10 == 0)
+                    tw.WriteLine();
+            }
+
+            tw.Close();
+        }
+    }
+}
