30b1d0bb500dffc5305a2a270c03263a7142d63f
svpetry <svpetry@gmail.com>
2010-11-28 09:50:05 +0000

    bugfixes

--- Z80/Z80_3DTest/3dmain.c
+++ Z80/Z80_3DTest/3dmain.c
@@ -11,51 +11,152 @@
 #include "defs.h"
 
 /******************************************************************************/
+
+const struct vector cube[8] = {
+	{-50, -50, 50},
+	{50, -50, 50},
+	{50, 50, 50},
+	{-50, 50, 50},
+	{-50, -50, -50},
+	{50, -50, -50},
+	{50, 50, -50},
+	{-50, 50, -50}
+};
+const byte cube_lines[12][2] = {
+	{0, 1},
+	{1, 2},
+	{2, 3},
+	{3, 0},
+	{4, 5},
+	{5, 6},
+	{6, 7},
+	{7, 4},
+	{0, 4},
+	{1, 5},
+	{2, 6},
+	{3, 7}
+};
+
+const struct vector zed[20] = {
+	{-40, -40, 18},
+	{40, -40, 18},
+	{40, -20, 18},
+	{-20, 40, 18},
+	{40, 40, 18},
+	{40, 60, 18},
+	{-40, 60, 18},
+	{-40, 40, 18},
+	{20, -20, 18},
+	{-40, -20, 18},
+	{-40, -40, -18},
+	{40, -40, -18},
+	{40, -20, -18},
+	{-20, 40, -18},
+	{40, 40, -18},
+	{40, 60, -18},
+	{-40, 60, -18},
+	{-40, 40, -18},
+	{20, -20, -18},
+	{-40, -20, -18}
+};
+const byte zed_lines[30][2] = {
+	{0, 1},
+	{1, 2},
+	{2, 3},
+	{3, 4},
+	{4, 5},
+	{5, 6},
+	{6, 7},
+	{7, 8},
+	{8, 9},
+	{9, 0},
+	{10, 11},
+	{11, 12},
+	{12, 13},
+	{13, 14},
+	{14, 15},
+	{15, 16},
+	{16, 17},
+	{17, 18},
+	{18, 19},
+	{19, 10},
+	{0, 10},
+	{1, 11},
+	{2, 12},
+	{3, 13},
+	{4, 14},
+	{5, 15},
+	{6, 16},
+	{7, 17},
+	{8, 18},
+	{9, 19},
+};
+
+
+/******************************************************************************/
 void start3d(void) {
 
-	struct vector v1, v2, v3, v4;
-	struct point p;
+	byte i;
+	struct point points[20];
+	struct point *p1, *p2;
 
-	v1.x = -40;
-	v1.y = -40;
-	v1.z = 0;
-
-	v2.x = 40;
-	v2.y = -40;
-	v2.z = 0;
-
-	v3.x = -40;
-	v3.y = 40;
-	v3.z = 0;
-
-	v4.x = 40;
-	v4.y = 40;
-	v4.z = 0;
+	int alpha = 0, beta = 0, gamma = 0;
 
 	while (io_read(128) == 255) {
 		clrbuf();
 
-		init_rotation(0, 0, 0);
+		init_rotation(alpha, beta, gamma);
 
-		rotate(&v1);
-		rotate(&v2);
-		rotate(&v3);
-		rotate(&v4);
+		for (i = 0; i < 8; i++)
+			vec2point(&cube[i], &points[i]);
 
-		vec2point(&v1, &p);
-		setpoint(&p);
-
-		vec2point(&v2, &p);
-		setpoint(&p);
-
-		vec2point(&v3, &p);
-		setpoint(&p);
-
-		vec2point(&v4, &p);
-		setpoint(&p);
+		for (i = 0; i < 12; i++) {
+			p1 = &points[cube_lines[i][0]];
+			p2 = &points[cube_lines[i][1]];
+			line(p1->x, p1->y, p2->x, p2->y);
+		}
 
 		buf2screen();
+
+		alpha += 10;
+		if (alpha >= 1024)
+			alpha = 0;
+		beta += 7;
+		if (beta >= 1024)
+			beta = 0;
+		gamma += 12;
+		if (gamma >= 1024)
+			gamma = 0;
 	}
+
+	alpha = beta = gamma = 0;
+	while (io_read(128) == 255) {
+		clrbuf();
+
+		init_rotation(alpha, beta, gamma);
+
+		for (i = 0; i < 20; i++)
+			vec2point(&zed[i], &points[i]);
+
+		for (i = 0; i < 30; i++) {
+			p1 = &points[zed_lines[i][0]];
+			p2 = &points[zed_lines[i][1]];
+			line(p1->x, p1->y, p2->x, p2->y);
+		}
+
+		buf2screen();
+
+		alpha += 10;
+		if (alpha >= 1024)
+			alpha = 0;
+		beta += 7;
+		if (beta >= 1024)
+			beta = 0;
+		gamma += 12;
+		if (gamma >= 1024)
+			gamma = 0;
+	}
+
 }
 /******************************************************************************/
 
